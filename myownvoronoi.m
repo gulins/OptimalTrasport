f9da9ca8392677f066b27014d72d0507a928cea0
Gulen <guleninsay@hotmail.co.uk>
2020-02-03 10:41:22 +0000

    Add description

--- myownvoronoi.m
+++ myownvoronoi.m
@@ -1,3 +1,7 @@
+%% Voronoi Diagram Generator
+% Partitioning of the plane using specified distance, dist, and specified parameters such as
+% number of sites N, weight of each site W and coordinates of each site sx and sy.
+%
 
 % number of sites
 N = 4;
@@ -30,9 +34,7 @@ V=zeros(length(y),length(x)); % voronoi diagram
 D=zeros(length(y),length(x)); % distances for each point
 
 % facility: vertical coordinate (on the plot), horizontal coordinate, weight
-%facilities = { {[3 3 1]}, {[78 39 0.6]}, {[50 80 1]}, {[120 20 3]}, {[120 50 1]} };
 facilities = [sites{1}' sites{2}' sites{3}'];
-%[ [3 3 1]; [78 39 0.6]; [50 80 1]; [120 20 3]; [120 50 1] ];
 
 
 for i=1:length(y)
@@ -47,7 +49,7 @@ for i=1:length(y)
             % DISTANCE
             p=2;
             dist=(((abs(y(i)-fy))^2+(abs(x(j)-fx))^2)^(p/2))-w;
-            %dist=(((abs(y(i)-fy))^p+(abs(x(j)-fx))^p)^(1/p))-w;
+            dist=(((abs(y(i)-fy))^p+(abs(x(j)-fx))^p)^(1/p))-w;
             
             if (dist<mindist)
                 mindist=dist;
@@ -59,18 +61,16 @@ for i=1:length(y)
     end
 end
 
-%V(D<1)=length(sites)+6;
 imagesc(V)
 set(gca,'Ydir','Normal')
 pbaspect([1 1 1])
 hold on
 
 viscircles([scaleX.*(facilities(:,1)) scaleY.*(facilities(:,2))], (scaleX+scaleY)*sqrt(facilities(:,3)),'Color','k', 'EnhanceVisibility',false);
-% scatter(scaleX.*(facilities(:,1)), scaleY.*(facilities(:,2)), 50*(scaleX+scaleY)*sqrt(facilities(:,3)),'ko');
+% scatter(scaleX.*(facilities(:,1)), scaleY.*(facilities(:,2)), 50*(scaleX+scaleY)*sqrt(facilities(:,3)),'ko'); % emphasise the weight of each loci.
 
 hold on
 scatter(scaleX.*(facilities(:,1)), scaleY.*(facilities(:,2)),'ko','filled')
- % returns a colormap containing 50 rows.
 colormap(summer);
 axis off;
 
